f7ca7fa363957e45446e31a54e42200f3af1aa21
Yanic Lehmann <54947460+Kiimbu@users.noreply.github.com>
2025-11-14 23:10:03 +0100

    commit

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS Customers(
-    CustomerID PRIMARY KEY,
+    CustomerID SERIAL PRIMARY KEY,
     Firstname VARCHAR(255),
     Lastname VARCHAR(255),
     Age INTEGER,
-    E-Mail VARCHAR(255),
+    EMail VARCHAR(255),
     Street VARCHAR(255),
     Housenumber INTEGER,
     City VARCHAR(255),
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS Customers(
 );
 
 CREATE TABLE IF NOT EXISTS Books(
-    BookID PRIMARY KEY,
+    BookID SERIAL PRIMARY KEY,
     ISBN INTEGER,
     Name VARCHAR(255),
     NumberOfPages INTEGER,
@@ -20,32 +20,25 @@ CREATE TABLE IF NOT EXISTS Books(
 );
 
 CREATE TABLE IF NOT EXISTS Author(
-    AuthorID PRIMARY KEY,
-    Fistname VARCHAR(255),
+    AuthorID SERIAL PRIMARY KEY,
+    Firstname VARCHAR(255),
     Lastname VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS Loans(
-    LoanID PRIMARY KEY,
+    LoanID SERIAL PRIMARY KEY,
     CustomerID INTEGER,
     BookID INTEGER,
     LoanStartDate DATE,
     LoanEndDate DATE
 );
 
-CREATE TABLE IF NOT EXISTS BookAuthor(
-    AuthorID INTEGER,
-    BookID INTEGER
-);
 
 ALTER TABLE Loans
 ADD CONSTRAINT Loans_fk_CustomerID FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID),
-ADD CONSTRAINT Loans_fk_BookID FOREIGN KEY(BookID) REFERENCES Books(BookID)
+ADD CONSTRAINT Loans_fk_BookID FOREIGN KEY(BookID) REFERENCES Books(BookID);
 
 ALTER TABLE Books
-ADD CONSTRAINT Books_fk_AuthorID FOREIGN KEY(AuthorID) REFERENCES Author(AuthorID)
+ADD CONSTRAINT Books_fk_AuthorID FOREIGN KEY(AuthorID) REFERENCES Author(AuthorID);
 
-ALTER TABLE BookAuthor
-ADD CONSTRAINT BookAuthor_fk_AuthorID FOREIGN KEY AuthorID REFERENCES Author(AuthorID),
-ADD CONSTRAINT BookAuthor_fk_BookID FOREIGN KEY BookID REFERENCES Books(BookID)
 
