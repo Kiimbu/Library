cfbe2c9b621889698174f990bd5079dd951284ff
Yanic Lehmann <54947460+Kiimbu@users.noreply.github.com>
2025-11-15 23:06:58 +0100

    commit

--- LibraryBackend.py
+++ LibraryBackend.py
@@ -3,13 +3,36 @@ import psycopg2
 conn = psycopg2.connect("host='Localhost' dbname='Library' user='postgres' password='postgres'")
 cur = conn.cursor()
 
-
+#Shows all Customers
 def Show_All_Customers():
-    cur.execute("SELECT * FROM Customers;")
+    cur.execute("SELECT * FROM Customers")
     customer = cur.fetchall()
     for i in customer:
         print(i)
 
+#Shows all Books
+def Show_All_Books():
+    cur.execute("SELECT * FROM Books")
+    books = cur.fetchall()
+    for i in books:
+        print(i)
+
+#Shows all Authors
+def Show_All_Author():
+    cur.execute("SELECT * FROM Authors")
+    author = cur.fetchall()
+    for i in author:
+        print(i)
+
+#Shows all Loans
+def Show_All_Loans():
+    cur.execute("SELECT * FROM Loans")
+    loans = cur.fetchall()
+    for i in loans:
+        print(i)    
+
+
+#Shows the infos of a Customer on the basis of the CustomerID
 def Show_Specific_Customer(customerID):
     cur.execute(f"SELECT * FROM Customers WHERE CustomerID = {customerID}")
     customer = cur.fetchall()
