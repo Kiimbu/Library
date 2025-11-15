bbe0c0dc61226ce931492b9aa8ae566b0d0ce9f6
Yanic Lehmann <54947460+Kiimbu@users.noreply.github.com>
2025-11-15 22:51:16 +0100

    commit

---
+++ LibraryBackend.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect("host='Localhost' dbname='Library' user='postgres' password='postgres'")
+cur = conn.cursor()
+
+
+def Show_All_Customers():
+    cur.execute("SELECT * FROM Customers;")
+    customer = cur.fetchall()
+    for i in customer:
+        print(i)
+
+def Show_Specific_Customer(customerID):
+    cur.execute(f"SELECT * FROM Customers WHERE CustomerID = {customerID}")
+    customer = cur.fetchall()
+    for i in customer:
+        print(i)
+
+#Show_Specific_Customer(customerID = input("Geben sie die ID des Kunden ein: "))
